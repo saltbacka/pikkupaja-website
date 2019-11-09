0456701231919c04ab3386b737f8dbecf9304c4c
Juri Saltbacka <juri@saltbacka.com>
2019-11-09 19:40:22 +0200

    päivitetty rekisteri ja tietosuojaseloste

--- pages/rekisteri-ja-tietosuojaseloste.js
+++ pages/rekisteri-ja-tietosuojaseloste.js
@@ -21,26 +21,27 @@ const Home = () => (
 
       <h1>Rekisteri- ja tietosuojaseloste</h1>
       <h2>
-        Tämä on Yrityksen henkilötietolain (10 ja 24 §) ja EU:n yleisen
+        Tämä on Pikkupajan henkilötietolain (10 ja 24 §) ja EU:n yleisen
         tietosuoja-asetuksen (GDPR) mukainen rekisteri- ja tietosuojaseloste.
-        Laadittu pp.kk.vvvv. Viimeisin muutos pp.kk.vvvv.
+        Laadittu 9.11.2019. Viimeisin muutos 9.11.2019.
       </h2>
 
       <h3>1. Rekisterinpitäjä</h3>
-      <p>Yritys, Osoite, 01234 Postitoimipaikka</p>
-      <p>Muut yhteystiedot</p>
+      <p>Pikkupaja, Jasmine Saltbacka, Tuiskunkatu 1 A 12, 33900 Tampere</p>
+      <p>+358 45 119 0290</p>
+      <p>jasmine@pikkupaja.fi</p>
+
       <h3>2. Rekisteristä vastaava yhteyshenkilö</h3>
-      <p>Etunimi Sukunimi, sähköposti, puhelinnumero</p>
-      <p>
-        Jos organisaatiossasi on tietosuojavastaava, hänen tietonsa tulevat
-        tähän. Muutoin kuka tahansa yhteyshenkilö.
-      </p>
+      <p>Jasmine Saltbacka</p>
+      <p>+358 45 119 0290</p>
+      <p>jasmine@pikkupaja.fi</p>
+
       <h3>3. Rekisterin nimi</h3>
       <p>
         Yrityksen asiakasrekisteri, markkinointirekisteri, sidosryhmärekisteri,
-        verkkopalvelun käyttäjärekisteri, jäsenrekisteri, työntekijärekisteri
-        tms.
+        verkkopalvelun käyttäjärekisteri ja jäsenrekisteri.
       </p>
+
       <h3>4. Oikeusperuste ja henkilötietojen käsittelyn tarkoitus</h3>
       <p>
         EU:n yleisen tietosuoja-asetuksen mukainen oikeusperuste henkilötietojen
@@ -51,9 +52,6 @@ const Home = () => (
           henkilön suostumus (dokumentoitu, vapaaehtoinen, yksilöity, tietoinen
           ja yksiselitteinen)
         </li>
-        <li>sopimus, jossa rekisteröity on osapuolena</li>
-        <li>laki (mikä)</li>
-        <li>julkisen tehtävän hoitaminen (mihin perustuu), tai</li>
         <li>
           rekisterinpitäjän oikeutettu etu (esim. asiakassuhde, työsuhde,
           jäsenyys).
@@ -61,23 +59,16 @@ const Home = () => (
       </ul>
       <p>
         Henkilötietojen käsittelyn tarkoitus on yhteydenpito asiakkaisiin,
-        asiakassuhteen ylläpito, markkinointi tms.
+        asiakassuhteen ylläpito ja markkinointi
       </p>
       <p>
         Tietoja ei käytetä automatisoituun päätöksentekoon tai profilointiin.{" "}
       </p>
+
       <h3>5. Rekisterin tietosisältö</h3>
       <p>
-        Rekisteriin tallennettavia tietoja ovat: henkilön nimi, asema,
-        yritys/organisaatio, yhteystiedot (puhelinnumero, sähköpostiosoite,
-        osoite), www-sivustojen osoitteet, verkkoyhteyden IP-osoite,
-        tunnukset/profiilit sosiaalisen median palveluissa, tiedot tilatuista
-        palveluista ja niiden muutoksista, laskutustiedot, muut
-        asiakassuhteeseen ja tilattuihin palveluihin liittyvät tiedot.
-      </p>
-      <p>
-        Kerro tässä myös tietojen säilytysaika, mikäli mahdollista. Kerro myös,
-        jos tiedot esimerkiksi anonymisoidaan tietyn ajan kuluttua.
+        Rekisteriin tallennettavia tietoja ovat: henkilön nimi, puhelinnumero ja
+        sähköpostiosoite
       </p>
 
       <h3>6. Säännönmukaiset tietolähteet</h3>
@@ -95,16 +86,6 @@ const Home = () => (
       <p>
         Tietoja ei luovuteta säännönmukaisesti muille tahoille. Tietoja voidaan
         julkaista siltä osin kuin niin on sovittu asiakkaan kanssa.
-      </p>
-
-      <p>
-        Tietoja voidaan siirtää rekisterinpitäjän toimesta myös EU:n tai ETA:n
-        ulkopuolelle.
-      </p>
-
-      <p>
-        Mikäli luovutat henkilötietoja eri tahoille, kerro tässä mahdolliset
-        vastaanottajat tai vastaanottajaryhmät.
       </p>
 
       <h3>8. Rekisterin suojauksen periaatteet</h3>
@@ -126,11 +107,11 @@ const Home = () => (
         rekisteriin tallennetut tietonsa ja vaatia mahdollisen virheellisen
         tiedon korjaamista tai puutteellisen tiedon täydentämistä. Mikäli
         henkilö haluaa tarkistaa hänestä tallennetut tiedot tai vaatia niihin
-        oikaisua, pyyntö tulee lähettää kirjallisesti rekisterinpitäjälle.
-        Rekisterinpitäjä voi pyytää tarvittaessa pyynnön esittäjää todistamaan
-        henkilöllisyytensä. Rekisterinpitäjä vastaa asiakkaalle EU:n
-        tietosuoja-asetuksessa säädetyssä ajassa (pääsääntöisesti kuukauden
-        kuluessa).
+        oikaisua, pyyntö tulee lähettää sähköpostilla tai kirjallisesti
+        rekisterinpitäjälle. Rekisterinpitäjä voi pyytää tarvittaessa pyynnön
+        esittäjää todistamaan henkilöllisyytensä. Rekisterinpitäjä vastaa
+        asiakkaalle EU:n tietosuoja-asetuksessa säädetyssä ajassa
+        (pääsääntöisesti kuukauden kuluessa).
       </p>
 
       <h3>10. Muut henkilötietojen käsittelyyn liittyvät oikeudet</h3>
@@ -139,14 +120,12 @@ const Home = () => (
         henkilötietojen poistamiseen rekisteristä ("oikeus tulla unohdetuksi").
         Niin ikään rekisteröidyillä on muut EU:n yleisen tietosuoja-asetuksen
         mukaiset oikeudet kuten henkilötietojen käsittelyn rajoittaminen
-        tietyissä tilanteissa. Pyynnöt tulee lähettää kirjallisesti
-        rekisterinpitäjälle. Rekisterinpitäjä voi pyytää tarvittaessa pyynnön
-        esittäjää todistamaan henkilöllisyytensä. Rekisterinpitäjä vastaa
-        asiakkaalle EU:n tietosuoja-asetuksessa säädetyssä ajassa
-        (pääsääntöisesti kuukauden kuluessa).
+        tietyissä tilanteissa. Pyynnöt tulee lähettää sähköpostilla tai
+        kirjallisesti rekisterinpitäjälle. Rekisterinpitäjä voi pyytää
+        tarvittaessa pyynnön esittäjää todistamaan henkilöllisyytensä.
+        Rekisterinpitäjä vastaa asiakkaalle EU:n tietosuoja-asetuksessa
+        säädetyssä ajassa (pääsääntöisesti kuukauden kuluessa).
       </p>
-      <br />
-      <br />
       <br />
       <br />
     </div>
