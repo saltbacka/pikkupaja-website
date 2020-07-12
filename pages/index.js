0d6b67705c247aaa8a03833e6928c0a1022cc7f3
Juri Saltbacka <juri@saltbacka.com>
2020-07-12 17:12:54 +0300

    fixed type

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ const Home = () => {
           mukaamme tekijöitä.
         </h1>
         <h2>
-          Kauppapaikka avataan myyjille vuode 2020 aikaa.{" "}
+          Kauppapaikka avataan myyjille vuoden 2020 aikaa.{" "}
           <strong>Täytä yhteystietosi</strong> niin pääset mukaan. Otamme sinuun
           yhteyttä pikkupajalta ja kerromme lisää.
         </h2>
